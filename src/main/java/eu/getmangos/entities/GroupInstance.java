7f1eb81ab8655f89c6877450c505c5418ba578eb
Warkdev <cedric.servais@outlook.com>
2020-11-22 14:51:48 +0100

    Fixing bud on the leaderguid typo

--- src/main/java/eu/getmangos/entities/GroupInstance.java
+++ src/main/java/eu/getmangos/entities/GroupInstance.java
@@ -10,10 +10,10 @@ import lombok.Data;
 @Table(name = "group_instance")
 @NamedQueries({
     @NamedQuery(name = "GroupInstance.findAll", query = "SELECT g FROM GroupInstance g"),
-    @NamedQuery(name = "GroupInstance.findById", query = "SELECT g FROM GroupInstance c WHERE g.groupInstancePK.leaderguid = :guid AND g.groupInstancePK.instance = :instance"),
+    @NamedQuery(name = "GroupInstance.findById", query = "SELECT g FROM GroupInstance g WHERE g.groupInstancePK.leaderguid = :guid AND g.groupInstancePK.instance = :instance"),
     @NamedQuery(name = "GroupInstance.delete", query = "DELETE FROM GroupInstance g WHERE g.groupInstancePK.leaderguid = :guid AND g.groupInstancePK.instance = :instance"),
     @NamedQuery(name = "GroupInstance.pack", query = "UPDATE GroupInstance g SET g.groupInstancePK.instance = :newinstance WHERE g.groupInstancePK.instance = :instance"),
-    @NamedQuery(name = "GroupInstance.findByLeaderGuid", query = "SELECT g FROM GroupInstance g WHERE g.groupInstancePK.leaderGuid = :leaderGuid"),
+    @NamedQuery(name = "GroupInstance.findByLeaderGuid", query = "SELECT g FROM GroupInstance g WHERE g.groupInstancePK.leaderguid = :leaderGuid"),
     @NamedQuery(name = "GroupInstance.findByInstance", query = "SELECT g FROM GroupInstance g WHERE g.groupInstancePK.instance = :instance"),
     @NamedQuery(name = "GroupInstance.findByPermanent", query = "SELECT g FROM GroupInstance g WHERE g.permanent = :permanent")})
 public class GroupInstance {
